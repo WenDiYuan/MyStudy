c429c563f4607b771f808a9517f087857cb97c57
WenDiYuan <2903813992@qq.com>
2020-01-28 17:05:27 +0800

    使用equals

--- src/XuanZheJieGou.java
+++ src/XuanZheJieGou.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 public class XuanZheJieGou {
 
 	public static void main(String[] args) {
-		System.out.println("请输入您的成绩");
 		Scanner sc=new Scanner(System.in);
+		System.out.println("请输入您的成绩");
 		float a=sc.nextFloat();
-		System.out.println("请输入您的性别，男生请按1，女生请按2");
-		Scanner cs=new Scanner(System.in);
-		int b=cs.nextInt();
+		System.out.println("请输入您的性别");
+		String xb=sc.next();
 		if(a<=10){
-			if(b==1){System.out.println("恭喜你进入到决赛，你被分配到男子组");}
+			if(xb.equals("男")){System.out.println("恭喜你进入到决赛，你被分配到男子组");}
 			else{System.out.println("恭喜你进入到决赛，你被分配到女子组");}
 		}
 		else{System.out.println("抱歉，你没有进入到决赛");}
