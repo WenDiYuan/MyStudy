24570def616a2296527cfce86764b2eb1db18ae9
WenDiYuan <2903813992@qq.com>
2020-01-29 19:21:50 +0800

    自己输入一个数

--- src/XunHuan2.java
+++ src/XunHuan2.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 
 public class XunHuan2 {
 
 	public static void main(String[] args) {
-	    for (int i = 2; i <= 100; i++) {
+		System.out.println("请输入一个数");
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();int n=0;
+	    for (int i = 2; i <= b; i++) {
 	    	            int a=1;
 	    	            for (int j = 2; j < i; j++) {
 	    	                if (0 == i % j) {
@@ -12,6 +17,8 @@ public class XunHuan2 {
 	    	            }
 	    	            if (a==1) {
 	    	                System.out.print(i + " ");
+	    	                n++;
+	    	                if(n%10==0){System.out.println("");}
 	    	            }
 	    	        }
 	}
