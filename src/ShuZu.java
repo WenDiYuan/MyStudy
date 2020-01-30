34652bdae04c6048a7bd8f89cf06b1e3be823d78
WenDiYuan <2903813992@qq.com>
2020-01-30 17:47:17 +0800

    用for输出数组每个元素

---
+++ src/ShuZu.java
@@ -0,0 +1,11 @@
+
+public class ShuZu {
+
+	public static void main(String[] args) {
+int[] arr={10,20,30,40};
+for(int i=0;i<arr.length;i++){
+System.out.println("数组中第"+i+"个元素为："+arr[i]);
+}
+	}
+	}
+
