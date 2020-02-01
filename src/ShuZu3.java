2cd42464e572d2e4a742ca07dee0ab59e2078e3f
WenDiYuan <2903813992@qq.com>
2020-02-01 17:49:29 +0800

    数组的排序

---
+++ src/ShuZu3.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+
+public class ShuZu3 {
+
+	public static void main(String[] args) {
+		int b;
+int[] ar={10,40,20,50,90};
+Arrays.sort(ar);
+for(b=0;b<ar.length;b++){
+	System.out.println(ar[b]);
+}
+
+	}
+
+}
