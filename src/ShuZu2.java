d0c9a7e316712beb9999d491924a34d781a3df1c
WenDiYuan <2903813992@qq.com>
2020-02-01 14:01:26 +0800

    数组的复制

---
+++ src/ShuZu2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ShuZu2 {
+
+	public static void main(String[] args) {
+		int b;
+int[] ary={10,20,30,40,50};
+int[] ary1=Arrays.copyOf(ary,6);
+for(b=0;b<ary1.length;b++){
+System.out.println(ary1[b]);}}
+
+}
